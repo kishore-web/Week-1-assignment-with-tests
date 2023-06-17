99c9b4ac406d60eef34a7f99949ace8fd6e374bb
Kishore Ch <kishu.559@gmail.com>
2023-06-17 13:14:43 +0530

    ease test1 completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,21 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
+function stringSort(str){
+  const strSplit = str.split("")
+  console.log(strSplit)
+  const strSort = strSplit.sort()
+  return strSort.join("")
+}
+console.log(stringSort("weffa"))
 
 function isAnagram(str1, str2) {
-
+    if(stringSort(str1.toLowerCase()) === stringSort(str2.toLowerCase())){
+      return true
+    } else {
+      return false
+    }
 }
+
 
 module.exports = isAnagram;
